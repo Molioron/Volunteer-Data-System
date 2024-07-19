c92fb177b6c4c4f8d4a86bc6371129e321b52c7c
Molioron <ronpre2201@gmail.com>
2024-07-19 18:54:18 +0300

    added some "add/remove entity" methods to the handler: add/remove user, add recruitment post. #3

--- VDS-Solution/VDS-Backend/Src/Models/VDS/VDbHandler.cs
+++ VDS-Solution/VDS-Backend/Src/Models/VDS/VDbHandler.cs
@@ -7,6 +7,9 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using VDS_Backend.Src.Models.VDS.Contexts;
+using VDS_Backend.Src.Models.VDS.Tables;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using VDS_Backend.Src.Models.VDS.DataTypes;
 
 namespace VDS_Backend.Src.Models.VDS
 {
@@ -18,7 +21,55 @@ namespace VDS_Backend.Src.Models.VDS
         // db context
         private VDSContext Context { get; set; }
 
-        public VDbHandler(VDSContext db) { Context = db; }
+        /// <summary>
+        /// Creates a new handler for the given context.
+        /// Ensures that the database is created.
+        /// </summary>
+        /// <param name="db">given context to the database.</param>
+        public VDbHandler(VDSContext db) 
+        {
+            Context = db;
+            Context.Database.EnsureCreated();
+        }
+
+        /// <summary>
+        /// Adds a user to the database from given input
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <param name="phoneNumber"></param>
+        /// <returns>true, if the user was added successfully, otherwise false.</returns>
+        public bool addUser(String firstName, String lastName, string email, string password, string phoneNumber)
+        {
+            User u = new User()
+            { FirstName = firstName, LastName = lastName,Email = email, Password = password, PhoneNumber = phoneNumber,
+            RecruitmentPosts = [], VolunteerPosts = []};
+
+            return AddIfNotExists(Context.Users, u) is not null;
+        }
+        /// <summary>
+        /// Removes a user from the database based on it's email primary key
+        /// </summary>
+        /// <param name="email">the email of the user.</param>
+        /// <returns>true if the user was removed successfully, otherwise false.</returns>
+        public bool removeUser(string email)
+        {
+            return RemoveIfNotExists(Context.Users, email);
+        }
+        public bool addRecruitmentPost(string userEmail, string title, string description, string address,
+            Location location, Job job, DateTime initialDate, DateTime lastDate)
+        {
+            var post = new RecruitmentPost()
+            { UserEmail = userEmail, Title = title, Description = description, Address = address, Location = location,
+            Job = job, InitialDate = initialDate, LastDate = lastDate};
+            return AddIfNotExists(Context.Recruitments, post) is not null;
+        }
+        //public bool removeRecruitmentPost() {  }
+        //public bool addVolunteerPost() { }
+        //public bool removeVolunteerPost() { }
+
 
         /// <summary>
         /// Adds an entity to the databse if and only if it does not already exist.
@@ -28,11 +79,34 @@ namespace VDS_Backend.Src.Models.VDS
         /// <param name="entity">the entity to add</param>
         /// <param name="predicate">additional predicate, an entity exists if it's in the table and satisfies this predicate.
         /// if predicate = null, then the predicate is ignored.</param>
-        /// <returns>The entry to this entity</returns>
-        public static EntityEntry<T> AddIfNotExists<T>(DbSet<T> dbSet, T entity, Expression<Func<T, bool>> predicate = null) where T : class, new()
+        /// <returns>The entry to this entity, or null if the entry exists already.</returns>
+        private EntityEntry<T>? AddIfNotExists<T>(DbSet<T> dbSet, T entity, Expression<Func<T, bool>> predicate = null) where T : class, new()
         {
             var exists = predicate != null ? dbSet.Any(predicate) : dbSet.Any();
-            return !exists ? dbSet.Add(entity) : null;
+            var res = !exists ? dbSet.Add(entity) : null;
+            if (res != null) { Context.SaveChanges(); } // save only if there is a change
+            return res;
+        }
+        /// <summary>
+        /// Removes an entity based on a given primary key.
+        /// </summary>
+        /// <typeparam name="T">type of entity.</typeparam>
+        /// <param name="dbSet">table the entity is in</param>
+        /// <param name="key">the primary key of the entity</param>
+        /// <returns>true if the entity was found and removed successfully, otherwise false</returns>
+        private bool RemoveIfNotExists<T>(DbSet<T> set, string key) where T : class, new()
+        {
+            // Find the entity by key
+            var entity = set.Find(key);
+
+            if (entity == null)
+            {
+                return false; // Entity does not exist
+            }
+
+            set.Remove(entity);
+            Context.SaveChanges();
+            return true; // Entity existed and was removed
         }
     }
 
--- VDS-Solution/VDS-Backend/Src/Program.cs
+++ VDS-Solution/VDS-Backend/Src/Program.cs
@@ -2,43 +2,22 @@
 using SQLitePCL;
 using VDS_Backend.Src.Models.VDS;
 using VDS_Backend.Src.Models.VDS.Contexts;
-using VDS_Backend.Src.Models.VDS.Tables;
+using VDS_Backend.Src.Models.VDS.DataTypes;
 
 
 // Initialize SQLitePCL
 Batteries_V2.Init();
 
-Console.WriteLine("Hello, World!");
-
 await using VDSContext db = new VDSContextSQLite();
+VDbHandler handler = new VDbHandler(db);
 
-db.Database.EnsureCreated();
+
 
 Console.WriteLine("adding data");
-User u1 = new User()
-{
-    FirstName = "John",
-    LastName = "Doe",
-    Email = "JohnDoe@gmail.com",
-    Password = "JohnDoe123",
-    PhoneNumber = "0000000000",
-    RecruitmentPosts = [],
-    VolunteerPosts = []
-};
-User u2 = new User()
-{
-    FirstName = "Jane",
-    LastName = "Doe",
-    Email = "JaneDoe@gmail.com",
-    Password = "JaneDoe123",
-    PhoneNumber = "1111111111",
-    RecruitmentPosts = [],
-    VolunteerPosts = []
-};
-VDbHandler.AddIfNotExists(db.Users, u1);
-VDbHandler.AddIfNotExists(db.Users, u2);
-
-db.SaveChanges();
+handler.addUser("John", "Doe", "JohnDoe@gmail.com", "JohnDoe123", "0000000000");
+handler.removeUser("JaneDoe@gmail.com");
+handler.addRecruitmentPost("JohnDoe@gmail.com", "Haifa tour instructor needed", "instructor needed asap", "Somewhere in Haifa",
+    Location.North, Job.Transportation, DateTime.Now, DateTime.Now.AddDays(1));
 
 var results_all = from user in db.Users
               select user;
@@ -46,6 +25,13 @@ var results_all = from user in db.Users
 var results = from user in db.Users
              where user.FirstName == "John"
              select user;
+
+var posts = from recruitment in db.Recruitments
+            select recruitment;
+
+var post_owners = from recruitment in db.Recruitments
+                 where recruitment.Id == 1
+                 select recruitment.User;
 
 Console.WriteLine("finding everyone:");
 foreach (var result in results_all)
@@ -57,4 +43,16 @@ Console.WriteLine("finding john:");
 foreach (var result in results)
 {
     Console.WriteLine($"{result.FirstName} {result.LastName}, mail: {result.Email}, phone: {result.PhoneNumber}");
+}
+
+Console.WriteLine("finding posts:");
+foreach (var result in posts)
+{
+    Console.WriteLine($"{result.Id}, {result.User.FirstName} {result.User.LastName}, {result.UserEmail}, {result.Title}, {result.Description}");
+}
+
+Console.WriteLine("finding post owner:");
+foreach (var result in post_owners)
+{
+    Console.WriteLine($"{result.FirstName} {result.LastName}");
 }
