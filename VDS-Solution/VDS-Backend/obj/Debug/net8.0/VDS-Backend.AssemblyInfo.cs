6362a84cd2ec24e0fc21a581d1c7dcb74959dbed
Emmanuel Heffes <ema.heff@gmail.com>
2024-08-18 09:14:25 +0300

    file that i need to commit in order to use pull command

--- VDS-Solution/VDS-Backend/obj/Debug/net8.0/VDS-Backend.AssemblyInfo.cs
+++ VDS-Solution/VDS-Backend/obj/Debug/net8.0/VDS-Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VDS-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0481813a2ecde785cdc11c567532a24201e20e6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8272423077529101b9c22830739b3cb65bcc1a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("VDS-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VDS-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
