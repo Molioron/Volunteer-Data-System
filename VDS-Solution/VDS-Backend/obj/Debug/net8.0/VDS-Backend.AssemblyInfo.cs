13c9149c9e7df998f0e0578d381676685472fe01
Emmanuel Heffes <ema.heff@gmail.com>
2024-08-26 16:07:59 +0300

    running changes

--- VDS-Solution/VDS-Backend/obj/Debug/net8.0/VDS-Backend.AssemblyInfo.cs
+++ VDS-Solution/VDS-Backend/obj/Debug/net8.0/VDS-Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VDS-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2790890ae4840f7cade49fabe66f56b794c77697")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f17dbb65575dc490c3b2bd2ee53d989fc2402a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("VDS-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VDS-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
